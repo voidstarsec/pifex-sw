9463024428b6f8f2533e62fe25d42d059cce68db
wrongbaud <50204068+wrongbaud@users.noreply.github.com>
2024-08-04 16:15:01 -0400

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,12 +4,10 @@
 sudo apt-get install build-essential vim tmux git screen flashrom i2c-tools can-utils minicom cmake ipython3 python3-pip urjtag binwalk openocd
 
 # Install LUMA
-pip3 install luma.oled
+sudo apt-get install  python3-luma.oled
 
 # Clone and install LUMA examples
 git clone https://github.com/rm-hull/luma.examples.git
-cd luma.examples
-sudo -H pip install -e .
 
 # Install Jupyter
-pip3 install jupyterlab
+pip3 install python3-jupyterlab
